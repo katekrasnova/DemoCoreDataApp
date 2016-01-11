e647d66e0f8d4fe39160a42ec3ac1656264a78f4
katekrasnova <katekrasnova1990@gmail.com>
2016-01-11 14:10:55 +0300

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by Екатерина Краснова on 11.01.16.
+//  Copyright © 2016 Kate Krasnova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreData
+//
+//  Created by Екатерина Краснова on 11.01.16.
+//  Copyright © 2016 Kate Krasnova. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
